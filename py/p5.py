910e4b31b1c0453f297d79aa40f2a0278ef9a818
DhulasiRaman <119476265+Dhulasiraman@users.noreply.github.com>
2024-03-16 10:24:47 +0530

    Update p5.py\

--- py/p5.py
+++ py/p5.py
@@ -1,17 +1,13 @@
-#recursive program to accept a number between 1 and 10 and print it
-
-def get_number():
-    val1 = input('Enter a number: ')
+ def get_number():
     try:
-        val1 = int(val1)
+        val1 = int(input('Enter a number: '))
         while val1 < 1 or val1 > 10:
-            val1 = input('Enter a number: ')
-            val1 = int(val1)
+            val1 = int(input('Enter a number: '))
 
         str_to_print = '{:.1f}'.format(val1)
         return str_to_print
 
     except ValueError:
-        get_number()
+        return get_number()
 
 print(get_number())
